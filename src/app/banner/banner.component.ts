a7ebd4245ffe60216c71a39c96b23df567e33d09
pratapdeb <pratapdeb1996@gmail.com>
2019-05-14 10:40:29 +0530

    5 files modified

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component ({
 
 selector:'banner',
@@ -10,7 +10,14 @@ styleUrls:[]
 
 export class BannerComponent{
 
-
+images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
 
 
