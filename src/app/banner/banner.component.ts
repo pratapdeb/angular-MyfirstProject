815c5d3d24ff415c6ea620d16086b33976ad04b4
pratapdeb <pratapdeb1996@gmail.com>
2019-05-14 09:55:43 +0530

    2 files modified, 2 added

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+
+@Component ({
+
+selector:'banner',
+templateUrl:'./banner.component.html',
+styleUrls:[]
+
+})
+
+export class BannerComponent{
+
+
+
+
+
+}
